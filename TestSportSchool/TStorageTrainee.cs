61eac6d0a654144449b048469ab2c3474e12983d
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-03 01:21:26 +0300

    установил пакет moq nuget для mock-тестирования и добавил тест на добавление ученика при корректных данных

--- TestSportSchool/TStorageTrainee.cs
+++ TestSportSchool/TStorageTrainee.cs
@@ -1,14 +1,37 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportSchoolLib;
 
 namespace TestSportSchool
 {
     [TestClass]
     public class TStorageTrainee
     {
+        private IStorageTrainee _storageTrainee;
+        private Trainee _testTrainee;
         [TestMethod]
-        public void TestMethod1()
+        public void TestAddTrainee_validData()
         {
+            var mockRep = new Mock<IStorageTrainee>();
+            var testTrainee = new TraineeDBManager(mockRep.Object);
+
+            var testValidTrainee = new Trainee
+            {
+                Id_Trainee = 1,
+                FIO = "Петров Пётр Петрович",
+                Birthday = new DateTime(1999, 1, 1),
+                Section = section.Спортивный_туризм,
+                Category = category.I_юношеский_спортивный_разряд,
+                FIOParent = "Петров Пётр Николаевич",
+                PhoneNumberParent = "88005553535"
+            };
+
+            mockRep.Setup(r => r.Id_TraineeExists(1))
+                   .Returns(false);
+            var actualResult = testTrainee.AddTrainee(testValidTrainee);
+
+            Assert.AreEqual("Ученик успешно добавлен", actualResult, "При успешном добавлении возвращается: Ученик успешно добавлен");
         }
     }
 }
