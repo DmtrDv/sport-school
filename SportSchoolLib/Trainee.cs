025feea8e67f19ab2951d1cba4f52aa768d22d2d
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-03 01:06:39 +0300

    добавил класс Trainee и описал его

---
+++ SportSchoolLib/Trainee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Collections.Specialized.BitVector32;
+
+namespace SportSchoolLib
+{
+    public class Trainee
+    {
+        public int Id_Trainee { get; set; }
+        public string FIO { get; set; }
+        public DateTime Birthday { get; set; }
+        public section Section { get; set; }
+        public category Category { get; set; }
+        public string FIOParent { get; set; }
+        public string PhoneNumberParent { get; set; }
+    }
+    public enum section
+    {
+        Спортивный_туризм,
+        скалолазание,
+        гребля
+
+    }
+    public enum category
+    {
+        III_юношеский_спортивный_разряд,
+        II_юношеский_спортивный_разряд,
+        I_юношеский_спортивный_разряд,
+        III_спортивный_разряд,
+        II_спортивный_разряд,
+        I_спортивный_разряд,
+        КМС,
+        МС,
+        отсутствует
+    }
+}
