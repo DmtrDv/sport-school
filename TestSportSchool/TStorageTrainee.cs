f5edae1212d8e85520df38228d6c91113fb37cbb
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-06 21:01:29 +0300

    добавил тесткейс на некорректную секцию

--- TestSportSchool/TStorageTrainee.cs
+++ TestSportSchool/TStorageTrainee.cs
@@ -75,5 +75,27 @@ namespace TestSportSchool
             Assert.AreEqual("Введите правильную дату", actualResult);
             mockRep.Verify(r => r.AddTrainee(It.IsAny<Trainee>()), Times.Never);
         }
+        [TestMethod]
+        public void TestAddTrainee_InvalidSection()
+        {
+            var mockRep = new Mock<IStorageTrainee>();
+            var testTrainee = new TraineeDBManager(mockRep.Object);
+
+            var testBirthdayInvalid = new Trainee
+            {
+                Id_Trainee = 1,
+                FIO = "Петров Пётр Петрович",
+                Birthday = new DateTime(1999, 12, 1),
+                Section = (section)1123456543,
+                Category = category.I_юношеский_спортивный_разряд,
+                FIOParent = "Петров Пётр Николаевич",
+                PhoneNumberParent = "88005553535"
+            };
+
+            var actualResult = testTrainee.AddTrainee(testBirthdayInvalid);
+
+            Assert.AreEqual("Выберите одну из существующих секций", actualResult);
+            mockRep.Verify(r => r.AddTrainee(It.IsAny<Trainee>()), Times.Never);
+        }
     }
 }
