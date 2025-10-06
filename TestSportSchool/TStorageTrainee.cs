336de0405ecddb49bdc6b7d5cd9ca613db2f00a9
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-06 21:18:29 +0300

    добавил тесткейс на пустое ФИО родителя

--- TestSportSchool/TStorageTrainee.cs
+++ TestSportSchool/TStorageTrainee.cs
@@ -119,5 +119,27 @@ namespace TestSportSchool
             Assert.AreEqual("Выберите один из существующих разрядов", actualResult);
             mockRep.Verify(r => r.AddTrainee(It.IsAny<Trainee>()), Times.Never);
         }
+        [TestMethod]
+        public void TestAddTrainee_emptyFIOParent()
+        {
+            var mockRep = new Mock<IStorageTrainee>();
+            var testTrainee = new TraineeDBManager(mockRep.Object);
+
+            var testValidTrainee = new Trainee
+            {
+                Id_Trainee = 1,
+                FIO = "Петров Пётр Петрович",
+                Birthday = new DateTime(1999, 1, 1),
+                Section = section.Спортивный_туризм,
+                Category = category.I_юношеский_спортивный_разряд,
+                FIOParent = "",
+                PhoneNumberParent = "88005553535"
+            };
+
+            var actualResult = testTrainee.AddTrainee(testValidTrainee);
+
+            Assert.AreEqual("Введите ФИО родителя", actualResult);
+            mockRep.Verify(r => r.AddTrainee(It.IsAny<Trainee>()), Times.Never);
+        }
     }
 }
