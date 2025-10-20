9e8077ddf83034fd378bdcd6884ad32756f86342
DmtrDv <dmitriybychkov75146@gmail.com>
2025-10-21 00:37:20 +0300

    вернул к изначальному варианту

--- SportSchoolLib/MySQLTraineeManager.cs
+++ SportSchoolLib/MySQLTraineeManager.cs
@@ -22,77 +22,29 @@ namespace SportSchoolLib
                 {
                     connection.Open();
 
-                    string sql = "SELECT Id_Trainee, FIO, Birthday, Section, Category, FIOParent, PhoneNumberParent FROM trainee";
+                string sql = "SELECT Id_Trainee, FIO, Birthday, Section, Category, FIOParent, PhoneNumberParent FROM trainee";
                     using (MySqlCommand command = new MySqlCommand(sql, connection))
                     using (MySqlDataReader reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Trainee trainee = new Trainee();
+                            int Id_trainee = reader.GetInt32("Id_Trainee");
 
-                            // Обработка Id_Trainee
-                            if (!reader.IsDBNull(reader.GetOrdinal("Id_Trainee")))
-                                trainee.Id_Trainee = reader.GetInt32("Id_Trainee");
-
-                            // Обработка FIO
-                            if (!reader.IsDBNull(reader.GetOrdinal("FIO")))
-                                trainee.FIO = reader.GetString("FIO");
-                            else
-                                trainee.FIO = string.Empty;
-
-                            // Обработка Birthday
-                            if (!reader.IsDBNull(reader.GetOrdinal("Birthday")))
-                                trainee.Birthday = reader.GetDateTime("Birthday");
-                            else
-                                trainee.Birthday = DateTime.MinValue;
-
-                            // Обработка Section
-                            if (!reader.IsDBNull(reader.GetOrdinal("Section")))
+                            Trainee trainee = new Trainee(Id_trainee);
+                            trainee.FIO = reader.GetString("FIO");
+                            trainee.Birthday = reader.GetDateTime("Birthday");
+                            string sectionString = reader.GetString("Section");
+                            if (Enum.TryParse<SportSchoolLib.section>(sectionString, true, out var section))
                             {
-                                string sectionString = reader.GetString("Section");
-                                if (Enum.TryParse<SportSchoolLib.section>(sectionString, true, out var section))
-                                {
-                                    trainee.Section = section;
-                                }
-                                else
-                                {
-                                    trainee.Section = section.Спортивный_туризм; // значение по умолчанию
-                                }
+                                trainee.Section = section;
                             }
-                            else
+                            string categoryString = reader.GetString("Category");
+                            if (Enum.TryParse<SportSchoolLib.category>(categoryString, true, out var category))
                             {
-                                trainee.Section = section.Спортивный_туризм; // значение по умолчанию
+                                trainee.Category = category;
                             }
-
-                            // Обработка Category
-                            if (!reader.IsDBNull(reader.GetOrdinal("Category")))
-                            {
-                                string categoryString = reader.GetString("Category");
-                                if (Enum.TryParse<SportSchoolLib.category>(categoryString, true, out var category))
-                                {
-                                    trainee.Category = category;
-                                }
-                                else
-                                {
-                                    trainee.Category = category.отсутствует; // значение по умолчанию
-                                }
-                            }
-                            else
-                            {
-                                trainee.Category = category.отсутствует; // значение по умолчанию
-                            }
-
-                            // Обработка FIOParent
-                            if (!reader.IsDBNull(reader.GetOrdinal("FIOParent")))
-                                trainee.FIOParent = reader.GetString("FIOParent");
-                            else
-                                trainee.FIOParent = string.Empty;
-
-                            // Обработка PhoneNumberParent
-                            if (!reader.IsDBNull(reader.GetOrdinal("PhoneNumberParent")))
-                                trainee.PhoneNumberParent = reader.GetString("PhoneNumberParent");
-                            else
-                                trainee.PhoneNumberParent = string.Empty;
+                            trainee.FIOParent = reader.GetString("FIOParent");
+                            trainee.PhoneNumberParent = reader.GetString("PhoneNumberParent");
 
                             trainees.Add(trainee);
                         }
@@ -105,7 +57,6 @@ namespace SportSchoolLib
             }
             return trainees;
         }
-
 
         public string AddTrainee(Trainee trainee)
         {
