3627d1d049b8f9b67c3e3e18c23552e082c5cad6
st56 <st56@gmail.com>
2025-11-01 12:31:47 +0300

    добавил класс SQLInstructorManager

---
+++ SportSchoolLib/SQLInstructorManager.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportSchoolLib
+{
+    public class SQLInstructorManager
+    {
+        MySqlConnection conn;
+
+        public List<Instructor> GetInstructors()
+        {
+            List<Instructor> result = new List<Instructor>();
+            return result;
+        }
+    }
+}
