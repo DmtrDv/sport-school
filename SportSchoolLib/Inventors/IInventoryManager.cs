7ee6fbc1478da7d79a08e61f0931b0ca72fa53d0
Daniil5903 <danilka7518@gmail.com>
2025-11-18 09:34:56 +0300

    добавил интерфейс IInventoryManager

---
+++ SportSchoolLib/Inventors/IInventoryManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportSchoolLib.Inventors
+{
+    public interface IInventoryManager
+    {
+        string AddInventory(Inventory inventory);
+    }
+}
