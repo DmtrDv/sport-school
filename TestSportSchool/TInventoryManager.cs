cf7a7986bf3c886da371b14e58d13571eca6b007
Daniil5903 <danilka7518@gmail.com>
2025-11-18 09:29:12 +0300

    добавил тестируемыё класс

---
+++ TestSportSchool/TInventoryManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSportSchool
+{
+    [TestClass]
+    public class TInventoryManager
+    {
+        [TestMethod]
+        public void TestAddInventory_validData()
+        {
+
+        }
+    }
+}
