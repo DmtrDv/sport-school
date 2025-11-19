d652177ef4390960c0f2de27f5d78164b8d78bb0
DmtrDv <dmitriybychkov75146@gmail.com>
2025-11-19 20:09:24 +0300

    реализовал метод получения списка инвентаря из БД

--- SportSchoolLib/Inventors/SQLInventoryManager.cs
+++ SportSchoolLib/Inventors/SQLInventoryManager.cs
@@ -3,11 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace SportSchoolLib.Inventors
 {
-    public class SQLInventoryManager
+    public class SQLInventoryManager: IInventoryManager
     {
+        MySqlConnection conn;
 
+        public List<Inventory> GetInventories()
+        {
+            List<Inventory> result = new List<Inventory>();
+
+            try
+            {
+                conn = new MySqlConnection(AppSettings.ConnectionString);
+                conn.Open();
+                const string query = "SELECT IdInventory, NameInventory, CountInventory, DateDelivery FROM inventory";
+                MySqlCommand command = new MySqlCommand(query, conn);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int Id = reader.GetInt32("IdInventory");
+
+                        Inventory inventory = new Inventory(Id);
+                        inventory.Name_Inventory = reader.GetString("NameInventory");
+                        inventory.Count_Inventory = reader.GetInt32("CountInventory");
+                        inventory.DateDelivery = reader.GetDateTime("DateDelivery");
+
+                        result.Add(inventory);
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                throw new Exception("Ошибка при загрузке инвентаря: " + ex.Message);
+            }
+            return result;
+        }
     }
 }
